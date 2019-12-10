85d46cb00af19aedd09d0f43b4c50576c1ad2256
bluelife <thebulelife@163.com>
2019-12-11 00:21:17 +0800

    .....

---
+++ applications/routers/api/member.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: bluelife
+ * @Date:   2019-12-11 00:18:46
+ * @Last Modified by:   bluelife
+ * @Last Modified time: 2019-12-11 00:18:55
+ */
+'use strict';
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const requestPromise = require('request-promise');
+const md5 = require('md5');
+const sillyDateTime = require('silly-datetime');
+const svgCaptcha = require('svg-captcha');
+const Mock = require('mockjs')
+const utils = require('../utils')
+const User = require('../models/user_model');
+const Item = require('../models/item_model');
+const Model = require('../models/model_model');
+const Interface = require('../models/interface_model');
+const Team = require('../models/team_model');
+const Logs = require('../models/logs_model');
+const MyDate = new Date();
+
+let output = {};
+
+router.use(function(req, res, next) {
+    output = {
+        code: 0,
+        msg: '',
+        ok: false,
+        data: null
+    };
+    next();
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -32,13 +32,14 @@ const sockerIO = require('socket.io')
 const flash = require('flash');
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const credentials = require('./credentials.js');
 const app = express()
 
 //是否启动记录访问日志
 const STARTLOG = true;
 const options = {
-	key: fs.readFileSync(path.join(__dirname + '/ssl/rap.mcloudhub.com.key')),
-	cert: fs.readFileSync(path.join(__dirname + '/ssl/rap.mcloudhub.com.pem'))
+    key: fs.readFileSync(path.join(__dirname + '/ssl/rap.mcloudhub.com.key')),
+    cert: fs.readFileSync(path.join(__dirname + '/ssl/rap.mcloudhub.com.pem'))
 };
 //设置模板引擎
 app.engine('html', swig.renderFile);
@@ -48,7 +49,7 @@ app.set('views', path.join(__dirname, '/applications/views'));
 app.set('view engine', 'html');
 // 将模板缓存设置false
 swig.setDefaults({
-	cache: false
+    cache: false
 });
 app.use(helmet());
 // 设置跨域
@@ -57,120 +58,121 @@ app.use(cors());
 app.use(expressRequestId);
 // extends设置true表示接收的数据是数组，false表示是字符串
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 // 将提交的数据转成json,并且设置请求实体大小
 app.use(bodyParser.json({
-	limit: '50mb'
+    limit: '50mb'
 }));
 app.use(bodyParser.urlencoded({
-	limit: '50mb',
-	extended: true
+    limit: '50mb',
+    extended: true
 }));
-app.use(cookieParser('session_id', {
-	maxAge: 1800000,
-	secure: true
+app.use(cookieParser(credentials.cookieSecret, {
+    maxAge: 1800000,
+    secure: true, // 设置cookie只通过安全连接(HTTPS)发送
+    httpOnly: true // 设置cookie 只能由服务器修改。也就是说客户端JavaScript不能修改它。这有助于防范XSS 攻击。
 }));
 // app.use(expressCurl);
 
 const store = new MongoDBStore({
-	uri: 'mongodb://localhost:27017',
-	databaseName: 'rap',
-	collection: 'sessions'
+    uri: 'mongodb://localhost:27017',
+    databaseName: 'rap',
+    collection: 'sessions'
 }, err => {
-	if (err) throw err;
+    if (err) throw err;
 });
 
 store.on('error', error => {
-	assert.ifError(error);
-	assert.ok(false);
+    assert.ifError(error);
+    assert.ok(false);
 });
 
 app.use(session({
-	genid: function(req) {
-		return uuidv4() // use UUIDs for session IDs
-	},
-	secret: 'session_id', // 与cookieParser中的一致
-	resave: true, // 设置强制刷新session
-	store: store, // 将session保存到mongodb中
-	saveUninitialized: false, // 是否保存未初始化的会话，如果是true则会保存许多session会导致保存有效session失败,一般设置为false.
-	cookie: {
-		secure: true,
-		maxAge: 1800000,
-	},
-	rolling: true
+    genid: function(req) {
+        return uuidv4() // use UUIDs for session IDs
+    },
+    secret: credentials.cookieSecret, // 与cookieParser中的一致
+    resave: true, // 设置强制刷新session
+    store: store, // 将session保存到mongodb中
+    saveUninitialized: false, // 是否保存未初始化的会话，如果是true则会保存许多session会导致保存有效session失败,一般设置为false.
+    cookie: {
+        secure: true,
+        maxAge: 1800000,
+    },
+    rolling: true
 }));
 
 app.use(flash());
 
 // 服务器启动时默认配置/动作
 app.use(function(req, res, next) {
-	// //将登录后的用户信息附加到request头信息中
-	if (req.cookies.uid && req.cookies.uid != '') {
-		try {
-			req.session.uid = req.cookies.uid;
-		} catch (e) {
-			console.log(e);
-		}
-	}
-	// 将系统类型添加到cookies和请求头中;
-	// os.platform return now node runing systems : darwin=>MAC win32=>windows
-	res.cookie('platform', os.platform);
-	req.platform = os.platform;
-	// flash a message
-	req.flash('info', 'hello!');
-	res.locals.errors = req.flash('error');
-	res.locals.infos = req.flash('info');
-	next();
+    // //将登录后的用户信息附加到request头信息中
+    if (req.cookies.uid && req.cookies.uid != '') {
+        try {
+            req.session.uid = req.cookies.uid;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    // 将系统类型添加到cookies和请求头中;
+    // os.platform return now node runing systems : darwin=>MAC win32=>windows
+    res.cookie('platform', os.platform);
+    req.platform = os.platform;
+    // flash a message
+    req.flash('info', 'hello!');
+    res.locals.errors = req.flash('error');
+    res.locals.infos = req.flash('info');
+    next();
 });
 
 app.use(expressWinston.logger({
-	transports: [
-		// 将日志信息打印在控制台
-		// new winston.transports.Console(),
-		new winston.transports.File({
-			filename: path.join(__dirname, 'logs/error.log'),
-			level: 'error'
-		}),
-		new winston.transports.File({
-			filename: path.join(__dirname, 'logs/combined.log')
-		})
-	],
-	format: winston.format.combine(winston.format.colorize(), winston.format.json()),
-	meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-	msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-	expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-	colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-	ignoreRoute: function(req, res) {
-		return false;
-	} // optional: allows to skip some log messages based on request and/or response
+    transports: [
+        // 将日志信息打印在控制台
+        // new winston.transports.Console(),
+        new winston.transports.File({
+            filename: path.join(__dirname, 'logs/error.log'),
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: path.join(__dirname, 'logs/combined.log')
+        })
+    ],
+    format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function(req, res) {
+        return false;
+    } // optional: allows to skip some log messages based on request and/or response
 }));
 
 app.use(csurf({
-	cookie: true,
-	ignoreMethods: ['GET', 'POST']
+    cookie: true,
+    ignoreMethods: ['GET', 'POST']
 }));
 app.use(function(err, req, res, next) {
-	if (err.code !== 'EBADCSRFTOKEN') return next(err)
-	// handle CSRF token errors here
-	res.status(403)
-	res.send('form tampered with')
+    if (err.code !== 'EBADCSRFTOKEN') return next(err)
+    // handle CSRF token errors here
+    res.status(403)
+    res.send('form tampered with')
 });
 
 // 记录访问日志
 if (STARTLOG) {
-	const logDirectory = path.join(__dirname, 'logs');
-	fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory); // 日志目录不存在创建目录
-	const logerFile = 'access_' + sillyDateTime.format(new Date(), 'YYYY_MMM_DD') + '.log';
-	const accessLogStream = rfs(logerFile, {
-		interval: '1d', // 日志切割间隔为1天，以s,m,h,d为单位
-		path: logDirectory, // 日志保存路径，
-		size: '1M', // 单个日志文件的大小，以B,K,M,G为单位
-		compress: true // 压缩日志
-	});
-	app.use(morgan('combined', {
-		stream: accessLogStream
-	}));
+    const logDirectory = path.join(__dirname, 'logs');
+    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory); // 日志目录不存在创建目录
+    const logerFile = 'access_' + sillyDateTime.format(new Date(), 'YYYY_MMM_DD') + '.log';
+    const accessLogStream = rfs(logerFile, {
+        interval: '1d', // 日志切割间隔为1天，以s,m,h,d为单位
+        path: logDirectory, // 日志保存路径，
+        size: '1M', // 单个日志文件的大小，以B,K,M,G为单位
+        compress: true // 压缩日志
+    });
+    app.use(morgan('combined', {
+        stream: accessLogStream
+    }));
 }
 
 //设置静态文件托管
@@ -190,30 +192,30 @@ app.use('/mock', require(path.join(__dirname, '/applications/routers/mock')));
 // });
 
 mongoose.connect('mongodb://localhost:27017/rap', {
-	useNewUrlParser: true
+    useNewUrlParser: true
 }, (err, res) => {
-	if (err) {
-		debug(err);
-	} else {
-		// 数据库连接成功后监听80/443端口
-		// app.listen(80);
-		http.createServer(app).listen(80);
-		// https.createServer(options, app).listen(443);
-		const server = http2.createServer(options, app);
-		const IO = sockerIO(server)
-		IO.on('connection', (socket) => {
-			console.log('socket client connected ....')
-			socket.on('minix', (data) => {
-				console.log(data)
-			})
+    if (err) {
+        debug(err);
+    } else {
+        // 数据库连接成功后监听80/443端口
+        // app.listen(80);
+        http.createServer(app).listen(80);
+        // https.createServer(options, app).listen(443);
+        const server = http2.createServer(options, app);
+        // const IO = sockerIO(server)
+        // IO.on('connection', (socket) => {
+        // 	console.log('socket client connected ....')
+        // 	socket.on('minix', (data) => {
+        // 		console.log(data)
+        // 	})
 
-			socket.on('disconnect', () => {
-				console.log('socket client disconneted .....')
-			})
-		})
-		server.listen(443);
+        // 	socket.on('disconnect', () => {
+        // 		console.log('socket client disconneted .....')
+        // 	})
+        // })
+        server.listen(443);
 
-	}
+    }
 });
 // app.listen(8080);
 module.exports = app;
